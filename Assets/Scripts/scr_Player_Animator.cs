43e98d617918c4c0443951dfedbc30869cab185d
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2024-08-21 15:52:52 +0300

    Animations

---
+++ Assets/Scripts/scr_Player_Animator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class scr_Player_Animator : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    public void SetIsForward(bool isMovingForward)
+    {
+        // Set the "IsForward" parameter in the animator
+        _animator.SetBool("IsForward", isMovingForward);
+    }
+
+    public void SetIsBackward(bool isMovingBackward)
+    {
+        // Set the "IsBackward" parameter in the animator
+        _animator.SetBool("IsBackward", isMovingBackward);
+    }
+}
--- Assets/Scripts/scr_Player_Movement.cs
+++ Assets/Scripts/scr_Player_Movement.cs
@@ -1,27 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class scr_Player_Movement : MonoBehaviour
 {
-    public CharacterController controller;  // Reference to the CharacterController component
-    public float speed = 6f;                // Speed of the player movement
-    public scr_UI_Joystick joystick;               // Reference to the on-screen joystick
+    public CharacterController controller;
+    public float speed = 6f;
+    public scr_UI_Joystick joystick;
+    private Vector3 moveDirection;
+    private scr_Player_Animator playerAnimator; // Reference to the player animator script
 
-    private Vector3 moveDirection;          // Direction in which the player moves
+    private void Start()
+    {
+        // Get the reference to the scr_Player_Animator component
+        playerAnimator = GetComponent<scr_Player_Animator>();
+    }
 
     void Update()
     {
-        // Get input from the joystick
         float horizontal = joystick.Horizontal;
         float vertical = joystick.Vertical;
 
-        // Calculate the direction to move in
-        moveDirection = new Vector3(horizontal, 0f, vertical);
+        moveDirection = new Vector3(-vertical, 0f, horizontal);
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
 
         // Move the player
         controller.Move(moveDirection * Time.deltaTime);
+
+        // Determine if the player is moving forward or backward
+        bool isMovingForward = horizontal > 0;
+        bool isMovingBackward = horizontal < 0;
+
+        // Update the animator with movement information
+        playerAnimator.SetIsForward(isMovingForward);
+        playerAnimator.SetIsBackward(isMovingBackward);
     }
 }
